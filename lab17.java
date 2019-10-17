c0983883a82932b427e9e8993fd7753dc04102a7
Nikhil Daga <53420562+nikhildaga007@users.noreply.github.com>
2019-10-17 11:44:54 +0530

    Add files via upload

---
+++ lab17.java
@@ -0,0 +1,22 @@
+import java.awt.*;
+import java.lang.*;
+
+class test extends Frame
+{
+	test()
+	{	
+		setTitle("BCA5");
+		setSize(400,400);
+		setBackground(Color.black);
+		setVisible(true);
+
+	}
+}
+
+class lab17
+{
+	public static void main (String[] args)
+	{
+		test obj= new test();
+	}
+}
